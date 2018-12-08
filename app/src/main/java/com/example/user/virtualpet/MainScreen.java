e6b43832924c8f48d1ad280ab13fa43a5aa13292
Gema Atta <atta2@illinois.edu>
2018-12-07 21:54:00 -0600

    managed to get the main screen TextViews to display the points, though it required repeating myself. Perhaps I'll figure out how to get a separate function to work (after the semester ends).

--- app/src/main/java/com/example/user/virtualpet/MainScreen.java
+++ app/src/main/java/com/example/user/virtualpet/MainScreen.java
@@ -8,21 +8,55 @@ import android.widget.TextView;
 
 public class MainScreen extends AppCompatActivity {
 
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_screen);
 
-        TextView nameTextView = (TextView) findViewById(R.id.nameTextView);
+        /* Displays the name set in activity_setup */
+        TextView nameTextView = findViewById(R.id.nameTextView);
         String setName = getIntent().getStringExtra("nameView");
         nameTextView.setText(setName);
+        final Pet pet = new Pet(setName);
+
+        /* Displays the happy and health points in appropriate TextViews */
+        final TextView happyPointsTV = findViewById(R.id.happyPointsTV);
+        final TextView healthPointsTV = findViewById(R.id.healthPointsTV);
 
         Button feedBtn = (Button) findViewById(R.id.feedBtn);
         feedBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 //display a new image for a certain amount of time
-                //Pet.feed();
+                pet.feed();
+                happyPointsTV.setText(pet.getHappy() + "");
+                healthPointsTV.setText(pet.getHealth() + "");
+            }
+        });
+
+        Button cleanBtn = findViewById(R.id.cleanBtn);
+        cleanBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //display a new image for a certain amount of time
+                pet.clean();
+                happyPointsTV.setText(pet.getHappy() + "");
+                healthPointsTV.setText(pet.getHealth() + "");
+                // Need to write a levelUp boolean function
+                // Function will pass new information to a level up activity
+            }
+        });
+
+        Button playBtn = findViewById(R.id.playBtn);
+        playBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //display a new image for a certain amount of time
+                pet.play();
+                happyPointsTV.setText(pet.getHappy() + "");
+                healthPointsTV.setText(pet.getHealth() + "");
+                // levelUp boolean activity
             }
         });
     }
--- app/src/main/java/com/example/user/virtualpet/Pet.java
+++ app/src/main/java/com/example/user/virtualpet/Pet.java
@@ -6,11 +6,15 @@ public class Pet implements Serializable {
     /** Name of the Pet. */
     private String name;
     /** Type of Pet. */
-    private Pet type;
+    private String type;
     /** Health points. */
     private int health;
     /** Happiness points. */
     private int happy;
+    /** Maximum happy points to level up. */
+    private int maxHappy;
+    /** Maximum health points to level up. */
+    private int maxHealth;
     /** Default constructor. */
     public Pet() {  }
     /** A new pet is made upon start up. */
@@ -18,9 +22,11 @@ public class Pet implements Serializable {
         // Name is set by the user.
         this.name = setName;
         // New pets start at level one.
-        this.type = new LevelOne();
+        this.type = "Level One";
         this.health = 0;
-        this. happy = 0;
+        this.happy = 0;
+        this.maxHappy = 30;
+        this.maxHealth = 30;
     }
 
     public int getHealth() {
@@ -35,9 +41,9 @@ public class Pet implements Serializable {
     public void setHappy(int setHappy) {
         this.happy = setHappy;
     }
+    public String getType() { return this.type; }
+
     public void feed() {
-        // something interactive should happen here...
-        // like the image changes to it eating for 3 seconds
         happy += 2;
         health += 2;
         levelUp(type);
@@ -52,28 +58,50 @@ public class Pet implements Serializable {
         happy += 5;
         levelUp(type);
     }
-    private void levelUp(Pet type) {
+    public boolean levelUp(String type) {
         /* 1. downcast to the appropriate Pet level.
          * 2. check if the happy and health meters have been reached.
          *      >> This is a separate boolean function in each level class.
          * 3. If true, the type is a new instance of the next level.
          * 4. happiness and health are reset.
          */
-        if (type instanceof LevelOne) {
-            LevelOne pet = (LevelOne) type;
-            if (pet.meter()) {
-                this.type = new LevelTwo();
+        if (type.equals("Level One")) {
+            if (meter()) {
+                this.type = "Level Two";
                 this.health = 0;
                 this.happy = 0;
+                this.maxHappy = 50;
+                this.maxHealth = 50;
+                return true;
             }
         }
-        if (type instanceof LevelTwo) {
-            LevelTwo pet = (LevelTwo) type;
-            if (pet.meter()) {
-                this.type = new LevelTwo();
+        if (type.equals("Level Two")) {
+            if (meter()) {
+                this.type = "Level Three";
                 this.health = 0;
                 this.happy = 0;
+                this.maxHappy = 100;
+                this.maxHealth = 100;
+                return true;
             }
         }
+        return false;
+    }
+    /**
+     * Check if the maximum happiness and health points have been met.
+     * Called by the levelUp() method in Pet parent class.
+     * Also resets the happiness and health points so that it never goes over the maximum.
+     */
+    private boolean meter() {
+        if (!(this.type.equals("Level Three")) && this.getHealth() > maxHealth) {
+            this.setHealth(maxHealth);
+        }
+        if (!(this.type.equals("Level Three")) && this.getHappy() > maxHappy) {
+            this.setHappy(maxHappy);
+        }
+        if (this.getHappy() >= maxHappy && this.getHealth() >= maxHealth) {
+            return true;
+        }
+        return false;
     }
 }
